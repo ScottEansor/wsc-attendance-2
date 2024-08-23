26fec5ead1bde73a6f4c0493db595a7935e971bc
scottyTwoskip <scottyeansor@gmail.com>
2024-08-23 11:07:39 -0600

    added athlete days missed count and list of days missed still need 3 more days div

--- frontend/src/Components/AthleteHistory.jsx
+++ frontend/src/Components/AthleteHistory.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect } from "react";
-
+// ------review use effect with Tim and talk about function for review. make sure its dialed
 const testAthletes = [
   { name: "Tyson", coach: "Coach Tony", date: "2024-08-01" },
   { name: "Winston", date: "2024-08-01" },
@@ -11,32 +11,35 @@ const testAthletes = [
   { name: "Grayson", coach: "Coach Tony", date: "2024-08-04" },
   { name: "Theo", coach: "Coach Scotty", date: "2024-08-04" },
   { name: "Stevey", coach: "Coach Tony", date: "2024-08-04" },
+  { name: "Winston", date: "2024-08-08" },
+  { name: "Winston", date: "2024-08-05" },
 ];
 
+//
 export default function AthleteHistory() {
   //test make sure data collection correct with Tim
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedAthlete, setSelectedAthlete] = useState("");
-  const [absentAthleteCount, setAbsentAthleteCount] = useState(0);
 
-  function displayAthleteHistory(selectedAthlete) {
-    let counter = 0;
+  const selectedAthleteAbsentDates = useMemo(() => {
+    let absentDates = [];
+
     // Loop through testAthletes to compare names and check if the athlete has a coach
     testAthletes.forEach((testAthlete) => {
       if (
         testAthlete.name.toLowerCase() === selectedAthlete.toLowerCase() &&
         !testAthlete.coach
       ) {
-        counter++;
+        absentDates.push(testAthlete.date);
       }
     });
-    setAbsentAthleteCount(counter);
-  }
+    return absentDates;
+  }, [selectedAthlete]);
 
   const filteredAthletes = useMemo(
     () => [
       ...new Set(
-        //set doesnt allow duplicate values in an array
+        //set doesnt allow duplicate values in an array/ also a data structure type
         testAthletes
           .filter((athlete) =>
             athlete.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -53,11 +56,12 @@ export default function AthleteHistory() {
     }
   }, [filteredAthletes]);
 
-  useEffect(() => {
-    if (selectedAthlete) {
-      displayAthleteHistory(selectedAthlete);
-    }
-  }, [selectedAthlete]);
+  // was just starting to try to solve the next div functionallity wise
+  // useEffect(() => {
+  //   if (absentAthleteCount > 3) {
+  //     displayAthleteHistory(selectedAthlete);
+  //   }
+  // }, [absentAthleteCount]);
 
   return (
     <div>
@@ -68,27 +72,30 @@ export default function AthleteHistory() {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <select
-        className="form-select"
-        value={selectedAthlete}
-        onChange={(e) => setSelectedAthlete(e.target.value)}
-      >
-        <option value="">Please select athlete</option>
-        {filteredAthletes.map((athlete) => {
-          return <option key={athlete}>{athlete}</option>;
-        })}
-      </select>
-      {selectedAthlete && (
-        <div>
-          {absentAthleteCount > 0 ? (
-            <p>
-              {selectedAthlete} has been absent {absentAthleteCount} times.
-            </p>
-          ) : (
-            <p>{selectedAthlete} has been present every day!</p>
-          )}
-        </div>
-      )}
+      <div>
+        <select
+          className="form-select"
+          value={selectedAthlete}
+          onChange={(e) => setSelectedAthlete(e.target.value)}
+        >
+          <option value="">Please select athlete</option>
+          {filteredAthletes.map((athlete) => {
+            return <option key={athlete}>{athlete}</option>;
+          })}
+        </select>
+        {selectedAthlete && (
+          <>
+            <div>Days missed: {selectedAthleteAbsentDates.length}</div>
+            <div>
+              {selectedAthleteAbsentDates.map((selectedAthleteAbsentDate) => (
+                <div key={selectedAthleteAbsentDate}>
+                  {selectedAthleteAbsentDate}
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
