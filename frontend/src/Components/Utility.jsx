107338f1c9fb799e1680236d952935a49193734b
scottyTwoskip <scottyeansor@gmail.com>
2024-09-12 15:52:34 -0600

    api frontend file

--- frontend/src/Components/Utility.jsx
+++ frontend/src/Components/Utility.jsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddAthlete from "./AddAthlete";
 import AddCoach from "./AddCoach";
 import AthleteHistory from "./AthleteHistory";
 import MissedAthletes from "./MissedAthletes.jsx";
 import "./utility.css";
-
-const testAthletes = [
-  { name: "Tyson", coach: "Coach Tony", date: "2024-08-01" },
-  { name: "Winston", date: "2024-08-01" },
-  { name: "Grayson", coach: "Coach Tony", date: "2024-08-01" },
-  { name: "Theo", date: "2024-08-01" },
-  { name: "Theo", date: "2024-08-01" },
-  { name: "Theo", date: "2024-08-01" },
-  { name: "Stevey", coach: "Coach Tony", date: "2024-08-01" },
-  { name: "Tyson", coach: "Coach Tony", date: "2024-08-04" },
-  { name: "Winston", date: "2024-08-04" },
-  { name: "Grayson", coach: "Coach Tony", date: "2024-08-04" },
-  { name: "Theo", coach: "Coach Scotty", date: "2024-08-04" },
-  { name: "Stevey", coach: "Coach Tony", date: "2024-08-04" },
-  { name: "Winston", date: "2024-08-08" },
-  { name: "Winston", date: "2024-08-05" },
-];
+import { getAttendance } from "../api.js";
 
 export default function Utility() {
+  const [attendance, setAttendance] = useState(null);
+
+  useEffect(() => {
+    let running = true;
+    async function fetchAttendance() {
+      const json = await getAttendance();
+      if (!running) return;
+      setAttendance(json);
+    }
+    fetchAttendance();
+    return () => {
+      running = false;
+    };
+  }, []);
+
+  if (attendance === null) {
+    return <div>Loading..</div>;
+  }
   return (
     <div className="utility">
       <div className="add-container d-flex flex-column align-items-center p-3 mb-2">
         <AddAthlete />
         <AddCoach />
       </div>
-      <AthleteHistory history={testAthletes} />
-      <MissedAthletes history={testAthletes} />
+      <AthleteHistory history={attendance} />
+      <MissedAthletes history={attendance} />
     </div>
   );
 }
---
+++ frontend/src/api.js
@@ -0,0 +1,19 @@
+export async function getAttendance() {
+    const testAthletes = [
+        { name: "Tyson", coach: "Coach Tony", date: "2024-08-01" },
+        { name: "Winston", date: "2024-08-01" },
+        { name: "Grayson", coach: "Coach Tony", date: "2024-08-01" },
+        { name: "Theo", date: "2024-08-01" },
+        { name: "Theo", date: "2024-08-01" },
+        { name: "Theo", date: "2024-08-01" },
+        { name: "Stevey", coach: "Coach Tony", date: "2024-08-01" },
+        { name: "Tyson", coach: "Coach Tony", date: "2024-08-04" },
+        { name: "Winston", date: "2024-08-04" },
+        { name: "Grayson", coach: "Coach Tony", date: "2024-08-04" },
+        { name: "Theo", coach: "Coach Scotty", date: "2024-08-04" },
+        { name: "Stevey", coach: "Coach Tony", date: "2024-08-04" },
+        { name: "Winston", date: "2024-08-08" },
+        { name: "Winston", date: "2024-08-05" },
+    ];
+    return testAthletes
+}
