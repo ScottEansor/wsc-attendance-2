9a5ed1d4b1ab54e7fcdfd4780f411d0274f0997c
scottyTwoskip <scottyeansor@gmail.com>
2024-10-08 08:29:12 -0600

    added logic to attendance.jsx, api.js, check with Tim

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config()
 
-// Middleware
-app.use(express.json());
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-// Simple route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const app = express()
+const Port = process.env.PORT || 5000;
 
-// Connect to MongoDB (replace <your_connection_string> with your actual MongoDB connection string)
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Connection error", error);
-  });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-// just basic.
+app.use(cors())
+app.use(express.json())
