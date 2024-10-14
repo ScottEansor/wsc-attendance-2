9d637495b8038e001fe41777d139c1cc32a4f628
scottyTwoskip <scottyeansor@gmail.com>
2024-10-14 15:09:58 -0600

    added real logic instead of testAttendance to getAttendance function inside the frontend api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -20,22 +20,40 @@ export async function saveAttendance(attendanceData) {
 }
 
 export async function getAttendance() {
-    const testAttendance = [
-        { name: "Tyson", coach: "Coach Tony", date: "2024-08-01" },
-        { name: "Winston", date: "2024-08-01" },
-        { name: "Grayson", coach: "Coach Tony", date: "2024-08-01" },
-        { name: "Theo", date: "2024-08-01" },
-        { name: "Theo", date: "2024-08-01" },
-        { name: "Theo", date: "2024-08-01" },
-        { name: "Stevey", coach: "Coach Tony", date: "2024-08-01" },
-        { name: "Tyson", coach: "Coach Tony", date: "2024-08-04" },
-        { name: "Winston", date: "2024-08-04" },
-        { name: "Grayson", coach: "Coach Tony", date: "2024-08-04" },
-        { name: "Theo", coach: "Coach Scotty", date: "2024-08-04" },
-        { name: "Stevey", coach: "Coach Tony", date: "2024-08-04" },
-        { name: "Winston", date: "2024-08-08" },
-        { name: "Winston", date: "2024-08-05" },
-    ];
-    return testAttendance
+    // const testAttendance = [
+    //     { name: "Tyson", coach: "Coach Tony", date: "2024-08-01" },
+    //     { name: "Winston", date: "2024-08-01" },
+    //     { name: "Grayson", coach: "Coach Tony", date: "2024-08-01" },
+    //     { name: "Theo", date: "2024-08-01" },
+    //     { name: "Theo", date: "2024-08-01" },
+    //     { name: "Theo", date: "2024-08-01" },
+    //     { name: "Stevey", coach: "Coach Tony", date: "2024-08-01" },
+    //     { name: "Tyson", coach: "Coach Tony", date: "2024-08-04" },
+    //     { name: "Winston", date: "2024-08-04" },
+    //     { name: "Grayson", coach: "Coach Tony", date: "2024-08-04" },
+    //     { name: "Theo", coach: "Coach Scotty", date: "2024-08-04" },
+    //     { name: "Stevey", coach: "Coach Tony", date: "2024-08-04" },
+    //     { name: "Winston", date: "2024-08-08" },
+    //     { name: "Winston", date: "2024-08-05" },
+    // ];
+
+    try {
+        const response = await fetch('/api/attendances', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error('failed to fetch attendance data')
+        }
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error('Error fetching attendance data:', error)
+        throw error
+    }
 }
 
