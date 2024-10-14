7a54407afac2103bbdc790801b3ca247fe0a5e0f
scottyTwoskip <scottyeansor@gmail.com>
2024-10-14 14:59:47 -0600

    finished first attempt at attendance route

--- backend/routes/attendance.js
+++ backend/routes/attendance.js
@@ -39,9 +39,37 @@ router.post('/', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
-    const { name, coach } = req.body
+    const { name, coach, date } = req.body
 
     try {
-        //stopped here
+        const attendanceRecord = await Attendance.findById(req.params.id)
+        if (!attendanceRecord) {
+            return res.status(404).json({ message: 'Attendance record not found' })
+        }
+        //put this in here so it only changes by one of these 3 > however, 
+        // i dont update anyhting yet. need to review front end undo and.. do i need a date? 
+        if (name) attendanceRecord.name = name
+        if (coach) attendanceRecord.coach = coach
+        if (date) attendanceRecord.date = date
+
+        const updatedAttendance = await attendanceRecord.save()
+        res.status(200).json(updatedAttendance)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const attendanceRecord = await Attendance.findById(req.params.id)
+
+        if (!attendanceRecord) {
+            return res.status(404).json({ message: 'Attendance record not found' })
+        }
+
+        await attendanceRecord.remove()
+        res.status(200).json({ mressage: 'Attendance record deleted' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
     }
 })
